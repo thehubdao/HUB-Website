8a10b28d3409712cca2f3cd85bdbcf7897f452f4
DAC-info <luca.eich@thedac.info>
2021-10-05 13:49:20 +0900

    WIP blog page

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -13,10 +13,10 @@ const BlogPage: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex flex-col items-center bg-white">
+      <main className="flex flex-col items-center bg-white h-screen">
 
         <div className="text-gray-200 p-10 max-w-full 2xl:max-w-screen-2xl">
-          <h1 className="text-5xl md:text-7xl font-bold mb-10">Blog</h1>
+          <h1 className="text-5xl md:text-7xl font-bold mb-10 text-black text-center">Cooming soon!</h1>
 
         </div>
 
