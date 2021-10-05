646b67183711060712edc9d533aae528e35df474
DAC-info <luca.eich@thedac.info>
2021-10-05 14:10:52 +0900

    refactoring

--- components/NFTCard.tsx
+++ components/NFTCard.tsx
@@ -5,7 +5,7 @@ const NFTCard = ({ img, title, link, classes }: any) => {
     return (
 
         <a href={link} target="_blank">
-            <div className={`${classes} group shadow-colorbottom relative hover:opacity-100 hover:z-50 scale-100 flex flex-col w-full xl:w-56 rounded-2xl bg-gray-400 bg-opacity-10 transform transition duration-300 ease-in-out hover:shadow-color border border-white border-opacity-20 hover:border-opacity-70 cursor-pointer`}>
+            <div className={`${classes} hover:scale-105 xl:hover:scale-120 opacity-90 group shadow-colorbottom relative hover:opacity-100 hover:z-50 scale-100 flex flex-col w-full xl:w-56 rounded-2xl bg-gray-400 bg-opacity-10 transform transition duration-300 ease-in-out hover:shadow-color border border-white border-opacity-20 hover:border-opacity-70 cursor-pointer`}>
 
                 <div className="relative text-center h-72 w-full pb-full">
                     <Image className="rounded-2xl object-cover" src={img} layout="fill" />
--- components/Press.tsx
+++ components/Press.tsx
@@ -1,5 +1,7 @@
 import BlogCard from "./BlogCard"
 
+import pressJson from "./press.json";
+
 
 const Press = () => {
 
@@ -20,10 +22,10 @@ const Press = () => {
                 </div>
 
                 <div className="w-full h-full xl:h-80 lg:w-3/5 grid grid-cols-1 md:grid-cols-2 xl:grid-rows-5 xl:grid-cols-1 place-content-stretch justify-items-center gap-5 p-0 py-8 sm:pl-8">
-                    <BlogCard classes="z-0 xl:scale-85 xl:opacity-90 xl:bg-gray-300" date="Aug 25" link="https://thedac.medium.com/big-news-coming-mgh-is-partnering-with-the-ai-expert-intelligent-trading-machines-to-develop-a-76c5a10dc015" title="Big news coming: MGH is partnering with the AI expert Intelligent Trading Machines to develop a unique NFT price oracle!" article="Algorithms are better people. Better than probation officers, study counselors or couple therapists, they predict which criminal will go back to jail, what grades freshmen will have or whether a marriage will fail or not..." />
-                    <BlogCard classes="z-10 xl:scale-90 xl:opacity-90 xl:bg-gray-300" date="Sep 1" link="https://thedac.medium.com/announcing-new-partnership-between-mgh-and-myreality-dao-to-develop-amazing-games-for-the-sandbox-3f1c3daa2515" title="Announcing new partnership between MGH and MyReality DAO to develop amazing games for The Sandbox" article="The Ethiopians say, “If spiders weave together, they can bind a lion.” That’s why Batman trusted Robin. Bud Spencer only hit it off with Terence Hill — and R2D2 and C-3PO constantly talked past each other..." />
-                    <BlogCard classes="z-20 xl:scale-95 xl:opacity-90 xl:bg-gray-300" date="Sep 7" link="https://thedac.medium.com/presenting-the-dac-your-one-stop-solution-to-successfully-build-launch-and-scale-your-dao-8339d56d2753" title="Presenting The DAC… your one-stop solution to successfully build, launch and scale your DAO" article="It’s the morning meeting. The CEO steps in and with a sad expression on his face he says: “I’m sorry to tell you this: But we failed — totally.” “WHAT?” The whole team shakes their heads in disbelief. “We’ve not even started!”..." />
-                    <BlogCard classes="z-30 xl:scale-100" date="Sep 21" link="https://thedac.medium.com/announcing-mgh-dao-token-sale-the-new-token-to-make-a-passive-income-from-your-nfts-38426dddbe83" title="Announcing MGH DAO’s Token Generation Event — Making NFTs Accessible to Everyone" article="Lao Tzu says: “A journey of a thousand miles begins with a single step” And now is the time to make your first step — the first step to stake your NFTs and generate network rewards with them..."/>
+                    <BlogCard classes="z-0 xl:scale-85 xl:opacity-90 xl:bg-gray-300" date={pressJson[0].date} link={pressJson[0].link} title={pressJson[0].title} article={pressJson[0].text} />
+                    <BlogCard classes="z-0 xl:scale-90 xl:opacity-90 xl:bg-gray-300" date={pressJson[1].date} link={pressJson[1].link} title={pressJson[1].title} article={pressJson[1].text} />
+                    <BlogCard classes="z-0 xl:scale-95 xl:opacity-90 xl:bg-gray-300" date={pressJson[2].date} link={pressJson[2].link} title={pressJson[2].title} article={pressJson[2].text} />
+                    <BlogCard classes="z-0 xl:scale-100" date={pressJson[3].date} link={pressJson[3].link} title={pressJson[3].title} article={pressJson[3].text} />
 
                     <div className="col-span-full z-50 max-w-xs m-auto text-center rounded-full px-6 py-2 bg-gray-400 bg-opacity-10 transform xl:translate-y-36 hover:scale-105 transition duration-300 ease-in-out hover:shadow-color border border-white border-opacity-20 hover:border-opacity-70 cursor-pointer">
                         <a href="https://thedac.medium.com" target="_blank">
