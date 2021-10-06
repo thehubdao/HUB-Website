dabde0c17ef14cfaf1b7c96749b8786d9727e4e0
DAC-info <luca.eich@thedac.info>
2021-10-06 17:44:15 +0900

    edit partnerlogos

--- components/Partner.tsx
+++ components/Partner.tsx
@@ -1,10 +1,10 @@
 import Image from "next/image";
 
-import chainlink_logo from "../public/images/Partner/chainlink_logo.png"
-import amazingblocks_logo from "../public/images/Partner/amazingblocks_logo.svg"
-import pec_logo from "../public/images/Partner/pec_logo.png"
+import chainlink_logo from "../public/images/Partner/chainlink.png"
+import amazingblocks_logo from "../public/images/Partner/amazingblocks.svg"
+import pec_logo from "../public/images/Partner/pec.png"
 import haytic_logo from "../public/images/Partner/HAYTIC.png"
-import itrm_logo from "../public/images/Partner/ITRM.png"
+import itrm_logo from "../public/images/Partner/itrm.png"
 
 
 const Partner = () => {
@@ -15,26 +15,26 @@ const Partner = () => {
             <div className="w-full">
                 <div className={`w-full flex flex-col items-center justify-start p-8`}>
 
-                    <span className={`col-span-full text-5xl sm:text-6xl lg:text-7xl font-bold text-transparent bg-clip-text bg-gradient-to-b z-10 from-gray-400 via-gray-400 to-gray-900 mb-10`}>Partners</span>
+                    <span className={`col-span-full text-5xl sm:text-6xl lg:text-7xl font-bold text-transparent bg-clip-text bg-gradient-to-b z-10 from-gray-200  to-gray-600 mb-10`}>Partners</span>
 
-                    <div className="relative w-full flex flex-col md:flex-row justify-center items-center">
-                        <div className="m-5 max-w-sm ">
-                            <Image src={chainlink_logo} className="object-scale-down h-36 filter grayscale hover:grayscale-0 transition ease-in-out duration-500 cursor-pointer"/>
+                    <div className="relative w-full flex flex-col md:flex-row space-y-10 md:space-y-0 space-x-0 md:space-x-10 lg:space-x-24 justify-evenly items-center">
+                        <div className="m-0 max-w-sm">
+                            <img src="/images/Partner/chainlink.png" className="object-scale-down w-52 h-auto md:w-auto md:h-28 max-w-10 filter grayscale hover:grayscale-0 transition ease-in-out duration-500 cursor-pointer" />
                         </div>
-                        <div className="m-5 max-w-sm ">
-                            <Image src={amazingblocks_logo} className="object-scale-down h-36 filter grayscale hover:grayscale-0 transition ease-in-out duration-500 cursor-pointer"/>
+                        <div className="m-0 max-w-sm">
+                            <img src="/images/Partner/amazingblocks.svg" className="object-scale-down w-52 h-auto md:w-auto md:h-28 filter grayscale hover:grayscale-0 transition ease-in-out duration-500 cursor-pointer" />
                         </div>
-                        <div className="m-5 max-w-sm ">
-                        <img src="/images/Partner/myreality_logo.png" className="object-scale-down h-36 filter grayscale hover:grayscale-0 transition ease-in-out duration-500 cursor-pointer"/>
+                        <div className="m-0 max-w-sm">
+                            <img src="/images/Partner/myreality.png" className="object-scale-down h-20 w-auto md:h-28 filter grayscale hover:grayscale-0 transition ease-in-out duration-500 cursor-pointer" />
                         </div>
-                        <div className="m-5 max-w-sm ">
-                            <Image src={pec_logo} className="object-scale-down h-36 filter grayscale hover:grayscale-0 transition ease-in-out duration-500 cursor-pointer"/>
+                        <div className="m-0 max-w-sm">
+                            <img src="/images/Partner/pec.png" className="object-scale-down h-20 w-auto md:h-28 filter grayscale hover:grayscale-0 transition ease-in-out duration-500 cursor-pointer" />
                         </div>
-                        <div className="m-5 max-w-sm ">
-                            <Image src={haytic_logo} className="object-scale-down h-36 filter grayscale hover:grayscale-0 transition ease-in-out duration-500 cursor-pointer"/>
-                        </div>
-                        <div className="m-5 max-w-sm ">
-                            <Image src={itrm_logo} className="object-scale-down h-36 filter grayscale hover:grayscale-0 transition ease-in-out duration-500 cursor-pointer"/>
+                        {/* <div className="m-5 max-w-sm ">
+                            <Image src={haytic_logo} className="object-scale-down h-28 filter grayscale hover:grayscale-0 transition ease-in-out duration-500 cursor-pointer"/>
+                        </div> */}
+                        <div className="m-0 max-w-sm">
+                            <img src="/images/Partner/itrm.png" className="object-scale-down h-20 w-auto md:h-28 filter grayscale hover:grayscale-0 transition ease-in-out duration-500 cursor-pointer" />
                         </div>
                     </div>
 
