f1e03cfa96ff11467f20edcba1f7a46bfc6de0a6
DAC-info <luca.eich@thedac.info>
2021-10-07 16:04:35 +0900

    minor design fixes

--- components/Question.tsx
+++ components/Question.tsx
@@ -12,7 +12,7 @@ const Question = ({ question, answer }: any) => {
 
                 <AiOutlineMinus className=" text-black text-opacity-80 text-3xl hidden peer-checked:block max-w-min peer-checked:mb-3" />
 
-                <p className="text-black opacity-80 font-medium text-2xl peer-checked:mb-3 ml-3 col-span-11 select-none">{question}</p>
+                <p className="text-black opacity-80 font-medium text-xl sm:text-2xl peer-checked:mb-3 ml-3 col-span-11 select-none">{question}</p>
 
                 <div className="hidden peer-checked:block text-black text-opacity-50 col-span-full whitespace-pre-wrap">
                     {answer}
--- pages/faq.tsx
+++ pages/faq.tsx
@@ -24,7 +24,7 @@ const FAQPage: NextPage = () => {
         <div className="flex flex-col items-center text-black opacity-90 p-2 sm:p-10 w-full 2xl:max-w-screen-2xl">
           <h1 className="text-5xl md:text-7xl py-2 font-bold text-transparent bg-clip-text bg-gradient-to-r z-10 from-green-500 to-yellow-500 text-center m-20">Any Questions?</h1>
 
-          <div className="flex flex-col divide-y divide-gray-300 w-full lg:w-3/5 items-center min-h-screen">
+          <div className="flex flex-col divide-y divide-gray-300 w-full lg:w-3/5 items-center min-h-screen pb-10">
 
             {faqJson.map((element, key) => {
               return (
