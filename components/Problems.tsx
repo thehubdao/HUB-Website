afeb63c1ce934cc772a781d6a52f01ebd1ed545f
DAC-info <luca.eich@thedac.info>
2021-10-09 22:04:00 +0900

    minor design changes

--- components/Problems.tsx
+++ components/Problems.tsx
@@ -6,25 +6,25 @@ const Problems = () => {
             <span className={`py-1 text-4xl sm:text-5xl xl:text-6xl font-bold text-transparent bg-clip-text bg-gradient-to-b z-10 from-gray-200 to-pink-600`}>Industry Pain Points</span>
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-10 justify-items-center">
                 <div className="relative flex flex-col items-center p-2 max-w-xs">
-                    <div className="absolute inset-1/4 w-2/4 h-2/4 bg-gradient-to-br from-pink-200 to-pink-800 rounded-full filter blur-2xl" />
+                    <div className="absolute inset-1/4 w-2/4 h-2/4 bg-gradient-to-br from-pink-200 to-pink-800 rounded-full filter blur-2xl opacity-75" />
                     <span className="text-white text-xl mb-3 p-1 z-10 border-b border-pink-500 max-w-max">Illiquidity</span>
                     <span className="text-gray-400 z-10">Lack of liquidity in the NFT market hampers the tradability and accessibility to this unique asset class</span>
                 </div>
 
                 <div className="relative flex flex-col items-center w-full p-2 max-w-xs">
-                    <div className="absolute inset-1/4 w-2/4 h-2/4 bg-gradient-to-br from-pink-200 to-pink-800 rounded-full filter blur-2xl" />
+                    <div className="absolute inset-1/4 w-2/4 h-2/4 bg-gradient-to-br from-pink-200 to-pink-800 rounded-full filter blur-2xl opacity-75" />
                     <span className="text-white text-lg mb-3 z-10 border-b border-pink-500 max-w-max">Inefficient Utilization</span>
                     <span className="text-gray-400 z-10">Majority of NFTs have a limited earning potential. NFTs stored in wallets do not provide additional value until sold for a higher price</span>
                 </div>
 
                 <div className="relative flex flex-col items-center w-full p-2 max-w-xs">
-                    <div className="absolute inset-1/4 w-2/4 h-2/4 bg-gradient-to-br from-pink-200 to-pink-800 rounded-full filter blur-2xl" />
+                    <div className="absolute inset-1/4 w-2/4 h-2/4 bg-gradient-to-br from-pink-200 to-pink-800 rounded-full filter blur-2xl opacity-75" />
                     <span className="text-white text-xl mb-3 z-10 border-b border-pink-500 max-w-max">Inaccurate Valuation</span>
                     <span className="text-gray-400 z-10">Intransparent, inflexible and fragmented; Emotionally driven due to a lack of valuation models</span>
                 </div>
 
                 <div className="relative flex flex-col items-center w-full p-2 max-w-xs">
-                    <div className="absolute inset-1/4 w-2/4 h-2/4 bg-gradient-to-br from-pink-200 to-pink-800 rounded-full filter blur-2xl" />
+                    <div className="absolute inset-1/4 w-2/4 h-2/4 bg-gradient-to-br from-pink-200 to-pink-800 rounded-full filter blur-2xl opacity-75" />
                     <span className="text-white text-xl mb-3 z-10 border-b border-pink-500 max-w-max">Limited Access</span>
                     <span className="text-gray-400 z-10">NFT datasets and their respective valuations are hard to retrieve</span>
                 </div>
--- components/Tokenomics.tsx
+++ components/Tokenomics.tsx
@@ -22,19 +22,19 @@ const Tokenomics = () => {
                     <div className="relative flex flex-col w-full lg:w-1/2 h-full space-y-12 justify-center items-center">
                         {/* <div className="z-10 border-yellow-300 border h-full w-full absolute top-0 left-0 bg-black bg-opacity-40 backdrop-filter backdrop-blur-2xl " /> */}
                         <div className="relative flex flex-col items-center w-full max-w-xs p-2">
-                            <div className="absolute inset-1/4 w-2/4 h-2/4 bg-gradient-to-br from-pink-500 to-blue-500 rounded-full filter blur-2xl" />
+                            <div className="absolute inset-1/4 w-2/4 h-2/4 bg-gradient-to-br from-pink-500 to-blue-500 rounded-full filter blur-2xl opacity-75" />
                             <span className="text-white text-xl mb-3 p-1 z-10 border-b border-pink-500 max-w-max">Sustainable Utility</span>
                             <span className="text-gray-400 z-10">Access to the MGH ecosystem, incentives for members and contributors and integration in various process workflows. Further, decentralized token allocation is providing a fair and sustainable environment for the community.</span>
                         </div>
 
                         <div className="relative flex flex-col items-center w-full max-w-xs p-2">
-                            <div className="absolute inset-1/4 w-2/4 h-2/4 bg-gradient-to-br from-pink-500 to-blue-500 rounded-full filter blur-2xl" />
+                            <div className="absolute inset-1/4 w-2/4 h-2/4 bg-gradient-to-br from-pink-500 to-blue-500 rounded-full filter blur-2xl opacity-75" />
                             <span className="text-white text-xl mb-3 z-10 border-b border-pink-500 max-w-max">Governance</span>
                             <span className="text-gray-400 z-10">Governance over network rewards, pool proposals, the pricing oracle and algorithm as well as NFT curation such as managing LANDS in the open metaverse.</span>
                         </div>
 
                         <div className="relative flex flex-col items-center w-full max-w-xs p-2">
-                            <div className="absolute inset-1/4 w-2/4 h-2/4 bg-gradient-to-br from-pink-500 to-blue-500 rounded-full filter blur-2xl" />
+                            <div className="absolute inset-1/4 w-2/4 h-2/4 bg-gradient-to-br from-pink-500 to-blue-500 rounded-full filter blur-2xl opacity-75" />
                             <span className="text-white text-xl mb-3 z-10 border-b border-pink-500 max-w-max">Data</span>
                             <span className="text-gray-400 z-10">You will be able to leverage the MGH token to gain access to datasets from sophisticated pricing algorithms analyzing NFTs as well as the pricing oracle.</span>
                         </div>
