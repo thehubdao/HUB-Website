59def38145b76a279cccfd176755c016c32798bc
DAC-info <luca.eich@thedac.info>
2021-10-13 16:03:00 +0900

    blog card add glass style

--- components/BlogCard.tsx
+++ components/BlogCard.tsx
@@ -1,7 +1,7 @@
 const BlogCard = ({title, article, date, link, classes}: any) => {
     return (
         <a href={link} target="_blank">
-        <div className={`${classes} m-auto relative hover:z-50 opacity-100 hover:opacity-100 hover:shadow-black scale-100 flex flex-col p-3 pt-5 h-full xl:h-56 w-full xl:w-3/4 rounded-2xl text-left shadow-colorbottom bg-gray-200 hover:bg-gray-100 bg-opacity-100 transform xl:-translate-y-16 hover:scale-105 transition duration-300 ease-in-out border border-white border-opacity-20 hover:border-opacity-70 cursor-pointer`}>
+        <div className={`${classes} m-auto relative hover:z-50 hover:shadow-black scale-100 flex flex-col p-3 pt-5 h-full xl:h-56 w-full xl:w-3/4 rounded-2xl text-left shadow-colorbottom bg-gray-200 bg-opacity-100 xl:bg-opacity-60 backdrop-filter backdrop-blur-md hover:backdrop-blur-xl transform xl:-translate-y-16 hover:scale-105 transition duration-300 ease-in-out border-l border-t border-white border-opacity-60 hover:border-opacity-70 cursor-pointer`}>
             
             <p className="text-black text-xl font-semibold">{title}</p>
             <p className="mt-3 text-black font-light">{article}</p>
--- components/Press.tsx
+++ components/Press.tsx
@@ -45,9 +45,9 @@ const Press = () => {
                 </div>
 
                 <div className="w-full h-full xl:h-80 lg:w-3/5 grid grid-cols-1 md:grid-cols-2 xl:grid-rows-5 xl:grid-cols-1 place-content-stretch justify-items-center gap-5 p-0 py-8 lg:pl-8">
-                    <BlogCard classes="z-0 xl:scale-85 xl:opacity-90 xl:bg-gray-300" date={pressJson[0].date} link={pressJson[0].link} title={pressJson[0].title} article={pressJson[0].text} />
-                    <BlogCard classes="z-0 xl:scale-90 xl:opacity-90 xl:bg-gray-300" date={pressJson[1].date} link={pressJson[1].link} title={pressJson[1].title} article={pressJson[1].text} />
-                    <BlogCard classes="z-0 xl:scale-95 xl:opacity-90 xl:bg-gray-300" date={pressJson[2].date} link={pressJson[2].link} title={pressJson[2].title} article={pressJson[2].text} />
+                    <BlogCard classes="z-0 xl:scale-85 " date={pressJson[0].date} link={pressJson[0].link} title={pressJson[0].title} article={pressJson[0].text} />
+                    <BlogCard classes="z-0 xl:scale-90 " date={pressJson[1].date} link={pressJson[1].link} title={pressJson[1].title} article={pressJson[1].text} />
+                    <BlogCard classes="z-0 xl:scale-95 " date={pressJson[2].date} link={pressJson[2].link} title={pressJson[2].title} article={pressJson[2].text} />
                     <BlogCard classes="z-0 xl:scale-100" date={pressJson[3].date} link={pressJson[3].link} title={pressJson[3].title} article={pressJson[3].text} />
 
                     <div className="col-span-full z-50 max-w-xs m-auto xl:translate-y-36 button">
