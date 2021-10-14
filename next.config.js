232ab9a622a4d786c11b7ed8c538eb097e630cbb
DAC-info <luca.eich@thedac.info>
2021-10-14 10:54:58 +0900

    add redirects

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  async redirects () {
+    return [
+      {
+        source: '/docs/mghdao_engl_whitepaper.pdf',
+        destination: '/docs/mgh_whitepaper_v3.pdf',
+        permanent: true,
+      },
+      {
+        source: '/wp-content/uploads/2021/06/mghdao_engl_whitepaper_20210626_print-1.pdf',
+        destination: '/docs/mgh_whitepaper_v3.pdf',
+        permanent: true,
+      }
+    ]
+  }
 };
