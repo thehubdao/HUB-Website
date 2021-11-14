aae6eacdf626d09ac9e50297a2a9a2164d4ffa82
DAC-info <luca.eich@thedac.info>
2021-11-14 10:43:08 +0900

    deploy

--- components/BlogCard.tsx
+++ components/BlogCard.tsx
@@ -1,4 +1,5 @@
 const BlogCard = ({ title, article, date, link, classes }: any) => {
+    
     return (
         <a href={link} target="_blank">
             <div className={`${classes} m-auto relative hover:z-50 hover:shadow-black scale-100 flex flex-col p-3 pt-5 h-full xl:h-56 w-full xl:w-3/4 rounded-2xl text-left shadow-colorbottom bg-gray-200 bg-opacity-100 xl:bg-opacity-60 backdrop-filter backdrop-blur-md hover:backdrop-blur-xl transform xl:-translate-y-16 hover:scale-105 transition duration-300 ease-in-out border-l border-t border-white border-opacity-60 hover:border-opacity-70 cursor-pointer`}>
