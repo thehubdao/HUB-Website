846c37bbabd2776785decf4001cf3c5f7e8501d6
DAC-info <84532190+DAC-info@users.noreply.github.com>
2021-12-02 18:12:15 +0000

    include second watcher (attempt)

--- pages/api/token.js
+++ pages/api/token.js
@@ -1,5 +1,5 @@
 import { createWatcher } from '@makerdao/multicall';
-const { tags, config, labels } = require('./config');
+const { tags, config, tags_polygon, config_polygon, labels } = require('./config');
 
 let watcher;
 
@@ -7,6 +7,8 @@ function run() {
     console.log("> initializing");
     watcher = createWatcher(tags, config);
     watcher.start();
+    watcher_polygon = createWatcher(tags_polygon, config_polygon);
+    watcher_polygon.start();
     console.log("> initialized");
 }
 
@@ -18,17 +20,23 @@ export default function handler(req, res) {
         console.log("> requesting", type);
         let result = {};
         watcher.subscribe(update => {
-            console.log(`Update: ${update.type} = ${update.value}`);
-            if (type === 'ALL' || type == 'CIRCULATING_SUPPLY') {
-                result[update.type] = update.value;
-                if (Object.keys(result).length == labels.length) {
-                    result['CIRCULATING_SUPPLY'] = calculateCirculatingSupply(result);
-                    if (type == 'CIRCULATING_SUPPLY')
-                        return res.status(200).json(result['CIRCULATING_SUPPLY']);
+            watcher_polygon.subscribe(update_polygon => {
+                console.log(`Update: ${update.type} = ${update.value}`);
+                console.log(`Update_polygon: ${update_polygon.type} = ${update_polygon.value}`);
+                if (type === 'ALL' || type == 'CIRCULATING_SUPPLY') {
+                    result[update.type] = update.value;
+                    if (Object.keys(result).length == labels.length) {
+                        if (type == 'CIRCULATING_SUPPLY') {
+                            result['CIRCULATING_SUPPLY'] = calculateCirculatingSupply(result);
+                            return res.status(200).json(result['CIRCULATING_SUPPLY']);
+                        }
+                    }
                     return res.status(200).json(result);
-                }
-            } else if (update.type === type)
-                return res.status(200).json(update.value);
+                } else if (update.type === type) {
+                    return res.status(200).json(update.value);
+                } else if (update_polygon.type === type) 
+                    return res.status(200).json(update_polygon.value);
+            }
         });
     } else
         return res.status(400).send({ err: "'" + type + "' is not a valid value type" });
@@ -42,6 +50,7 @@ function isValidValue(type) {
 }
 
 function calculateCirculatingSupply(data) {
-    let lockedSupply = data['VESTING'] + data['TREASURY'] + data['MULTISIG'] + data['LP'] + data['OLD_SALE'] + data['NEW_SALE'] + data['POLYGON_SUPPLY'];
+    let lockedSupply = data['VESTING'] + data['TREASURY'] + data['MULTISIG'] + data['LP'] + data['OLD_SALE'] + data['NEW_SALE'] + data['POLYGON_STAKING_BALANCE'] +
+                       data['POLYGON_TREASURY'] + data['POLYGON_LP'] - data['POLYGON_STAKING_SUPPLY';
     return data['TOTAL_SUPPLY'] - lockedSupply;
 }
