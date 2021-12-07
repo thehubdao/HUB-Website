5c4c890b10df84dfd2634a1354ea88db086277b2
DAC-info <luca.eich@thedac.info>
2021-12-08 01:51:56 +0900

    add redirect to app for land valuation

--- next.config.js
+++ next.config.js
@@ -12,6 +12,11 @@ module.exports = {
         source: '/wp-content/uploads/2021/06/mghdao_engl_whitepaper_20210626_print-1.pdf',
         destination: '/docs/mgh_whitepaper_v3.pdf',
         permanent: true,
+      },
+      {
+        source: '/valuation',
+        destination: 'https://app.metagamehub.io/valuation',
+        permanent: true,
       }
     ]
   }
