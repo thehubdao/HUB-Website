6c04c4254e667da85d41be01d1e36fdd4cffac31
DAC-info <luca.eich@thedac.info>
2022-01-05 21:16:53 +0700

    fix typo

--- components/Governance.tsx
+++ components/Governance.tsx
@@ -16,7 +16,7 @@ const Governance = ({ scroll }: any) => {
                     <div className="flex flex-col items-center lg:items-end 2xl:items-center text-center lg:text-right 2xl:text-center w-full">
 
                         <h2 className={`text-transparent bg-clip-text bg-gradient-to-br from-blue-500 to-pink-500 mb-5`}>MGH Governance</h2>
-                        <p className="long-text">Members of the MGH DAO have the power to vote on which NFT Pools are created, the terms of each pool (e.g., how much MGH has to be staked to access them) and the management of the DAO´s funds.</p>
+                        <p className="long-text">Members of the MGH DAO have the power to vote on which NFT Pools are created, the terms of each pool (e.g. how much MGH has to be staked to access them) and the management of the DAO´s funds.</p>
                         
                         <div className="max-w-xs self-center lg:self-end 2xl:self-center mt-10 button">
                             <a href="https://snapshot.org/#/metagamehub.eth" target="_blank">
