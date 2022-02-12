9f692c9d881257005b809d2c3708b8682be369fc
DAC-info <info@thedac.info>
2022-02-12 01:08:22 +0100

    compile test

--- components/section-element/TokenomicsPart.tsx
+++ components/section-element/TokenomicsPart.tsx
@@ -12,11 +12,11 @@ const TokenomicsPart = ({title, size, color, distance}: TokenomicsPartProps) => 
     const borderColorClass = `border-${color}`
 
     return (
-        <div className={`flex space-x-0 absolute ${distanceClass} ${sizeClass} w-96`}>
-            <div className={`border-8 ${borderColorClass}`} />
+        <div className={`flex space-x-0 absolute  w-96`}>
+            <div className={`border-8 `} />
             <div className={`flex space-x-3 pt-1 bg-blue-00/20 min-w-max items-center w-full pl-10`}>
                 <p className="text-2xl font-medium min-w-max">{title}</p>
-                <p className={`${textColorClass} text-lg font-medium`}>{size}%</p>
+                <p className={` text-lg font-medium`}>{size}%</p>
             </div>
         </div>
     )
