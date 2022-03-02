2097575a0a01ee6ca61318cfcd80fcbc1c010191
DAC-info <info@thedac.info>
2022-03-02 20:37:11 +0100

    remove tracking snippet

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -27,20 +27,6 @@ class MyDocument extends Document {
                         }}
                     />
 
-                    <script
-                        dangerouslySetInnerHTML={{
-                            __html: `
-                            (function(h,o,t,j,a,r){
-                                h.hj = h.hj || function () { (h.hj.q = h.hj.q || []).push(arguments) };
-                                h._hjSettings={hjid:2850691,hjsv:6};
-                                a=o.getElementsByTagName('head')[0];
-                                r=o.createElement('script');r.async=1;
-                                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-                                a.appendChild(r);
-                            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');ƒ
-                        `,
-                        }}
-                    />
                     <meta property="og:image" content="/images/Logos/MGH/mgh_logo.png" />
                     <meta property="og:image:type" content="image/png" />
                     <meta property="og:image:width" content="2664" />
