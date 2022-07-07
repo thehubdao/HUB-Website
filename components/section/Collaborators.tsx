e56f7fd39c42f3ab5e5c6d7f5c61ace0957f1fbf
Andres <andgonzalezcas@unal.edu.co>
2022-07-07 11:22:13 -0500

    changed box bg color on Network section(homepage), and add a margin reducing the width to 75%

--- components/section/Collaborators.tsx
+++ components/section/Collaborators.tsx
@@ -11,7 +11,7 @@ const Collaborators = () => {
         <h2 className={`font-normal`}>NETWORK</h2>
       </div>
 
-      <div className="relative mt-10 lg:mt-0 w-full grow-0 lg:grow grid grid-cols-2 xs:grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 gap-2">
+      <div className="relative mt-10 md:mt-0 w-full lg:w-4/5 grow-0 lg:grow grid grid-cols-2 xs:grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 gap-2">
         {collaboratorJSON.map((element, key) => {
           return <Partner key={key} link={element.link} logo={element.logo} />;
         })}
