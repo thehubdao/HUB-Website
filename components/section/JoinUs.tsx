4a7dedcf081cc5c76a0b7e2dcbfe18994ed73617
Andres <andgonzalezcas@unal.edu.co>
2022-07-07 11:57:49 -0500

    added responsive grid to the Join us secction (homepage)

--- components/section/JoinUs.tsx
+++ components/section/JoinUs.tsx
@@ -35,11 +35,11 @@ const JoinUs = () => {
     <div className="text-center min-h-screen flex items-center justify-center">
       <div>
         <h3 className="mb-8 text-xl text-pink-reco">Working Groups</h3>
-        <div className="flex justify-center gap-4 mb-8">
+        <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-5 justify-items-center gap-4 mb-8">
           {Object.keys(links).map((key) => (
             <a
               key={key}
-              className="p-6 bg-[#292929] transition-all hover:scale-110"
+              className=" w-28 p-6 bg-[#292929] transition-all hover:scale-110"
               href={links[key as keyof typeof links].link}
             >
               {links[key as keyof typeof links].icon}
