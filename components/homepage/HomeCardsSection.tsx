938dd2fc3efdde24d530a68c856ca219a8675477
Andres <andgonzalezcas@unal.edu.co>
2022-08-09 16:49:23 -0500

    added fixed responsive cards

--- components/homepage/HomeCardsSection.tsx
+++ components/homepage/HomeCardsSection.tsx
@@ -60,7 +60,7 @@ const HomeCardsSection = () => {
       </ul>
       <JoinUs />
       {/* Vertical Cards */}
-      <ul className="flex flex-col md:flex-row w-full items-center xl:justify-center md:justify-around gap-16 xl:gap-8 mb-16 sm:mb-0 flex-wrap">
+      <ul className="flex flex-col md:flex-row md:flex-wrap w-full items-center xl:justify-center md:justify-around gap-16 xl:gap-8 mb-16 sm:mb-0">
         {verticalCards.map(({ title, text, buttonText, link, src }) => (
           <li key={title}>
             <HomeVerticalCard
